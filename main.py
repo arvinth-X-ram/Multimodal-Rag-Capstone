3a3ce82bd1ccc0437d8ec8d62038a16a956c1e0a
RaghulSarav <raghulsaravanan1010@gmail.com>
2026-04-12 19:35:56 +0000

    feat: frontend using streamlit

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from capstone2-nexus!")
+from fastapi import FastAPI
+app=FastAPI()
 
+from src.api.v1.routes import admin,query
 
-if __name__ == "__main__":
-    main()
+@app.get("/health-check")
+def health_check():
+    return{
+        "msg":"Project Running"
+    }
+
+app.include_router(admin.router,prefix="/api/v1")
+app.include_router(query.router,prefix="/api/v1")
